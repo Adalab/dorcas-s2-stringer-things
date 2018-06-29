fcb2ca6c313847f1e223d23700fc290604e9faad
Raquel Romo <rqlrom@gmail.com>
2018-06-29 20:54:52 +0200

    js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log('>> Ready :)');
+var inputName = document.querySelector('.personal-name');
+var cardName = document.querySelector('.card__top--name');
+var activableSectionDesign = document.querySelector('.design__title');
+var SectionHiddenDesign = document.querySelector('.design__form');
+var valueFillInput;
+
+function reWriteName() {
+  valueFillInput = inputName.value;
+  cardName.innerHTML = valueFillInput;
+}
+
+inputName.addEventListener('change', reWriteName);
+
+function collapsibleDesign() {
+  // Si contiene la clase hidden la elimina y sino la añade
+  SectionHiddenDesign.classList.toggle('hidden');
+}
+
+activableSectionDesign.addEventListener('click', collapsibleDesign);
