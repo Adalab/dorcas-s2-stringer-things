4be4d1ec8178fa209cc46cad06f17f26a82b779b
Raquel Romo <rqlrom@gmail.com>
2018-07-03 20:58:24 +0200

    collapsibles I

--- js/main.js
+++ js/main.js
@@ -1,39 +1,30 @@
 'use strict';
 
-var inputName = document.querySelector('.js-personal-name');
-var cardName = document.querySelector('.js-card__top--name');
+// var inputName = document.querySelector('.js-personal-name');
+// var cardName = document.querySelector('.js-card__top--name');
 var activableSection = document.querySelectorAll('.js-collapsible-title');
-var SectionHidden = document.querySelectorAll('.js-collapsible-form');
-var valueFillInput;
+var parent;
 
-function reWriteName() {
-  valueFillInput = inputName.value;
-  cardName.innerHTML = valueFillInput;
-}
+console.log(activableSection);
 
-inputName.addEventListener('change', reWriteName);
+// var valueFillInput;
+//
+// function reWriteName() {
+//   valueFillInput = inputName.value;
+//   cardName.innerHTML = valueFillInput;
+// }
+//
+// inputName.addEventListener('change', reWriteName);
 
 function collapsibleDesign(event) {
-  var parent = event.currentTarget.parentElement;
-
-  // console.dir(parent);
-
+  parent = event.currentTarget.parentElement;
+  console.log(parent);
   // Si contiene la clase hidden la elimina y sino la añade
-  parent.classList.toggle('hidden');
+  parent.classList.toggle('js-collapsible-visible');
+
 }
 
 for (var i = 0; i < activableSection.length; i++) {
   activableSection[i].addEventListener('click', collapsibleDesign);
+  console.log(activableSection[i]);
 }
-
-
-
-
-
-// for (var i = 0; i < fruits.length; i++) {
-//   fruits[i].addEventListener('click', showAlertWithFruit);
-// }
-
-// function showAlertWithFruit(event){
-//   alert(event.currentTarget.innerHTML);
-// }
