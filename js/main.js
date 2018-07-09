8270c63f651c42e9c2259f4b5b6755c631e9511e
Loreto Vaquero <loret.vaquero@gmail.com>
2018-07-09 15:56:48 +0200

    merge push again

--- js/main.js
+++ js/main.js
@@ -1,34 +1,5 @@
 'use strict';
 
-var inputName = document.querySelector('.personal-name');
-var inputJobTitle = document.querySelector('.job-title');
-
-// var inputPhoto = document.querySelector('.fill-input__image');
-// var cardPhoto = document.querySelectSor('.card__image');
-// var activableSectionDesign = document.querySelector('.design__title');
-// var SectionHiddenDesign = document.querySelector('.design__form');
-
-// variables rrss
-// var mobile = document.querySelector('#phone');s
-// var mail = document.querySelector('#emailaddress');
-// var linkedin = document.querySelector('#lkdn');
-// var github = document.querySelector('#gh');
-
-
-function linkSM(e){
-  var fillLink = e.currentTarget;
-  var targetLink = fillLink.getAttribute('data-things');
-  var hrefNasi = '';
-  if (fillLink.type===email.type){
-    document.querySelector('#' + targetLink).href = 'mailto:'+fillLink.value;
-  } else if(fillLink.type===number){
-    document.querySelector('#' + targetLink).href = 'tel:'+fillLink.value;
-  } else {
-    document.querySelector('#' + targetLink).href += fillLink.value;
-    //linkedin.setAttribute("href", (fdocument.querySelector('#' + targetLink));
-  }
-}
-
 var itemsSocialMedia = document.querySelectorAll('.fill-input__placeholder');
 var uploadBtn = document.querySelector('.fill-input__image');
 var uploadInput = document.querySelector('.fill-input__input');
