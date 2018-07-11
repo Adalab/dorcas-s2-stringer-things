e47809dd2190fd350664902d1aec838f14406bc7
Raquel Romo <Raquel@MacBook-Pro-de-Eduardo.local>
2018-07-11 11:51:24 +0200

    revisar estos cambios, chungo

--- js/main.js
+++ js/main.js
@@ -73,22 +73,37 @@ var buttonSkills = document.querySelectorAll('.js-button-abilities');
 var boxSkills = document.querySelectorAll('.js-ability-box');
 var dataType;
 var dataIndex = 0;
+var contador = 0;
+var dataIndexPlus = 1;
+
 
 function createSkill(event){
   for (var i = 0; i < boxSkills.length; i++){
     dataType = event.currentTarget.getAttribute('data-type');
     dataIndex = event.currentTarget.getAttribute('data-index');
     dataIndex = parseInt(dataIndex);
-    console.log('dataType' ,dataType);
-    console.log('dataIndex type' ,typeof dataIndex);
-    if(dataType === 'hidden') {
+    // console.log('dataType' ,dataType);
+    // console.log('dataIndex' ,dataIndex);
+    // console.log('dataIndex type' ,typeof dataIndex);
+    if((boxSkills[dataIndex].classList.contains('hidden'))) {
+      console.log(boxSkills[dataIndex].classList.contains('hidden'));
+      console.log('menos');
+      // boxSkills[dataIndex].classList.add('hidden', 'plus');
       boxSkills[dataIndex].classList.add(dataType);
-    } else if (dataType === 'plus'){
-      console.log('dataIndex' ,dataIndex);
-      dataIndex = dataIndex + 1;
-      boxSkills[dataIndex].classList.add(dataType);
-    }
+    } else if ((boxSkills[dataIndex].classList.contains('plus'))){
+      console.log('mas');
+      // console.log('dataIndex' ,dataIndex);
+      boxSkills[dataIndexPlus].classList.add(dataType);
+     } //else {
+  //     alert('El dataType no existe');
+  //   }
+  // }
+  for (var i = 0; i < 2; i++){
+    dataIndexPlus = contador + 1;
   }
+  console.log('contador', contador);
+  contador++;
+}
 }
 
 for(var i = 0; i < buttonSkills.length; i++){
