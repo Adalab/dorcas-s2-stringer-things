901139f71c6caa9db951493537cba28b73207151
Loreto Vaquero <loret.vaquero@gmail.com>
2018-07-13 23:01:39 +0200

    Add card creator

--- js/main.js
+++ js/main.js
@@ -140,38 +140,38 @@ fillSelect();
 // //   }
 // }
 
-for (var i = 0; i < selectSkills.length; i++) {
-  selectSkills[i].addEventListener('change', addSelectSkills);
-}
+// for (var i = 0; i < selectSkills.length; i++) {
+//   selectSkills[i].addEventListener('change', addSelectSkills);
+// }
 
 //colapsables
-var activableSection = document.querySelectorAll(".js-collapsible-title");
-var parent;
-var fieldset = document.querySelectorAll(".js-collapsible-visible");
+// var activableSection = document.querySelectorAll(".js-collapsible-title");
+// var parent;
+// var fieldset = document.querySelectorAll(".js-collapsible-visible");
 
 //abre y cierra colapsables
 //console.log(activableSection);
 
-function collapsibleDesign(event) {
-  parent = event.currentTarget.parentElement;
-  //console.log(parent);
-  closeCollapsibles();
-  // Si contiene la clase hidden la elimina y sino la añade
-  parent.classList.toggle("js-collapsible-visible");
-}
-
-for (var i = 0; i < activableSection.length; i++) {
-  activableSection[i].addEventListener("click", collapsibleDesign);
-  //console.log(activableSection[i]);
-}
-// un colapsable abierto cada vez
-
-function closeCollapsibles() {
-  for (var i = 0; i < fieldset.length; i++) {
-    //console.log(activableSection.length);
-    fieldset[i].classList.remove("js-collapsible-visible");
-  }
-}
+// function collapsibleDesign(event) {
+//   parent = event.currentTarget.parentElement;
+//   //console.log(parent);
+//   closeCollapsibles();
+//   // Si contiene la clase hidden la elimina y sino la añade
+//   parent.classList.toggle("js-collapsible-visible");
+// }
+//
+// for (var i = 0; i < activableSection.length; i++) {
+//   activableSection[i].addEventListener("click", collapsibleDesign);
+//   //console.log(activableSection[i]);
+// }
+// // un colapsable abierto cada vez
+//
+// function closeCollapsibles() {
+//   for (var i = 0; i < fieldset.length; i++) {
+//     //console.log(activableSection.length);
+//     fieldset[i].classList.remove("js-collapsible-visible");
+//   }
+// }
 
 //actualizar colores
 var radioButtonColor = document.querySelectorAll(".color-check");
--- js/shareTwitter.js
+++ js/shareTwitter.js
@@ -8,51 +8,32 @@ var responseURL = document.querySelector('.phrase');
 
 
 function getJSONFromInputs(inputs) {
-  return inputs.reduce(function(acc, val) {
+  console.log(inputs);
+  return inputs.reduce(function (acc, val) {
+    console.log(val.nodeName);
     if (val.type==='radio' && val.checked===true) {
       acc[val.name] = val.value;
     }
-      if ((val.nodeName !== 'BUTTON') && (val.nodeName !== 'FIELDSET') && (val.nodeName !== 'SELECT'))
-        acc[val.name] = val.value;
-        return acc;
-      }, {})
-  }
+    if ((val.nodeName !== 'BUTTON') && (val.nodeName !== 'FIELDSET') && (val.type!=='radio') ) {
+      acc[val.name] = val.value;
+    }
+    return acc;
+  }, {});
+}
 
-  unction getJSONFromInputs(inputs){
-    console.log('inputs', inputs);
-    // return inputs.reduce(function (acc, val) {
-    //   if(val.nodeName !== 'BUTTON')
-    //     acc[val.name] = val.value;
-    //   return acc;
-
-    return inputs.reduce(function (acc, val) {
-     console.log(val.nodeName);
-
-
-     if (val.type==='radio' && val.checked===true) {
-       acc[val.name] = val.value;
-     }
-     if ((val.nodeName !== 'BUTTON') && (val.nodeName !== 'FIELDSET') && (val.type!=='radio') ){
-       acc[val.name] = val.value;
-     }
-
-     return acc;
-    }, {});
-  }
 
 function sendData() {
   var inputs = Array.from(form.elements);
-  //console.log(inputs);
   var json = getJSONFromInputs(inputs);
   json.skills = ['Javascript', ' React'];
   json.photo = photoFileReader.result;
-  //console.log(json);
   delete json[''];
   console.log(json);
   sendRequest(json);
 }
 
 function loadPhoto() {
+  console.log(btnCreateCard);
   var addedPhoto = document.querySelector('#add-image').files[0];
   photoFileReader.addEventListener('load', sendData);
   photoFileReader.readAsDataURL(addedPhoto);
@@ -86,4 +67,3 @@ function showURL(result) {
 }
 
 btnCreateCard.addEventListener('click', loadPhoto);
-console.log(btnCreateCard);
