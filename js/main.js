225b8b98ddaac9e7e75bf43abb6b533dcd457049
Raquel Romo <Raquel@MacBook-Pro-de-Eduardo.local>
2018-07-13 15:47:57 +0200

    insert options

--- js/main.js
+++ js/main.js
@@ -3,6 +3,33 @@
 var buttonAdd = document.querySelector('.js-button-abilitiesPlus');
 var buttonRemove = document.querySelectorAll('.js-button-abilitiesMinus');
 var boxSkills = document.querySelectorAll('.js-ability-box');
+var selectSkills = document.querySelectorAll('.js-select-abilities');
+
+function createOption(text){
+  var option = document.createElement('option');
+  var optionText = document.createTextNode(text);
+  option.appendChild(optionText);
+  return option;
+}
+
+function fillSelect() {
+  var url = 'https://raw.githubusercontent.com/Adalab/dorcas-s2-proyecto-data/master/skills.json';
+  fetch(url)
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(json){
+      var skillList =json.skills;
+      console.log(skillList);
+      for (var i = 0; i < selectSkills.length; i++){
+        for(var j = 0; j < skillList.length; j++){
+          var newSkillsOption = createOption(skillList[j]);
+          console.log(newSkillsOption);
+          selectSkills[i].appendChild(newSkillsOption);
+        }
+      }
+    });
+}
 
 function addSkill(){
   var boxHidden = document.querySelectorAll('.hidden');
@@ -24,3 +51,5 @@ function removeSkill(event){
 buttonAdd.addEventListener('click', addSkill);
 buttonRemove[0].addEventListener('click', removeSkill);
 buttonRemove[1].addEventListener('click', removeSkill);
+
+fillSelect();
