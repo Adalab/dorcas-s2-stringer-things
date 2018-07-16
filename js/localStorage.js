a1122a40e6de40a1d1553377de5259e09a94bd34
Loreto Vaquero <loret.vaquero@gmail.com>
2018-07-16 17:58:36 +0200

    working localStorage

--- js/localStorage.js
+++ js/localStorage.js
@@ -1,17 +1,24 @@
 'use strict';
 
 var lSname = document.querySelector('#nmb');
+var lSnameCard = document.querySelector('#fullname');
 var lSjob = document.querySelector('#job');
+var lSjobCard = document.querySelector('#profesion');
 var lSimg = document.querySelector('#add-image');
 var lSmail = document.querySelector('#emailaddress');
+var lSmailCard = document.querySelector('#mail-link');
 var lSphone = document.querySelector('#phone');
+var lSphoneCard = document.querySelector('#mobile-link');
 var lSlinkedin = document.querySelector('#lkdn');
+var lSlinkedinCard = document.querySelector('#linkedin-link');
 var lSgit = document.querySelector('#GH');
+var lSgitCard = document.querySelector('#github-link');
+
 
 var dataCard = {};
 
 function saveLocalStorage() {
-  console.log('hola');
+  console.dir(lSgitCard);
   if (!localStorage.getItem('Business-Card')) {
   dataCard = {
     'name': '',
@@ -21,6 +28,13 @@ function saveLocalStorage() {
     'telephone': '',
     'linkedin': '',
     'gitHub': '',
+    'nameCard': '',
+    'occupationCard': '',
+    'imageCard': '',
+    'emailCard': '',
+    'telephoneCard': '',
+    'linkedinCard': '',
+    'gitHubCard': '',
   }
 } else {
   dataCard = JSON.parse(localStorage.getItem('Business-Card'));
@@ -34,13 +48,23 @@ function saveLocalStorage() {
   dataCard.linkedin = lSlinkedin.value;
   dataCard.gitHub = lSgit.value;
 
+  dataCard.nameCard = lSnameCard.innerHTML;
+  dataCard.occupationCard = lSjobCard.innerHTML;
+  //dataCard.imageCard = lSimgCard.value;
+  dataCard.emailCard = lSmailCard.value;
+  dataCard.telephoneCard = lSphoneCard.value;
+  dataCard.linkedinCard = lSlinkedin.href;
+  dataCard.lSgitCard = lSgit.href;
+
   localStorage.setItem('Business-Card', JSON.stringify(dataCard));
 }
 
 function recoverLocalStorage () {
   dataCard = JSON.parse(localStorage.getItem('Business-Card'));
   lSname.value = dataCard.name;
+  lSnameCard.innerHTML = dataCard.occupationCard;
   lSjob.value = dataCard.occupation;
+  lSjobCard.innerHTML = dataCard.occupationCard;
   uploadPhoto.style.backgroundImage = "'url("+dataCard.image+")'";
     //miniBox.style.backgroundImage = infoTarjeta.foto;
   lSmail.value = dataCard.email;
