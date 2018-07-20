ed99657db17a01cbfcab80cb722b3298a890c123
Laura <laurav.zarco@gmail.com>
2018-07-20 20:53:10 +0200

    card preview to ES6

--- js/main.js
+++ js/main.js
@@ -2,59 +2,59 @@
 
 //Preview card
 
-var itemsSocialMedia = document.querySelectorAll('.fill-input__placeholder');
-var uploadBtn = document.querySelector('.fill-input__image');
-var uploadInput = document.querySelector('.fill-input__input');
-var uploadPhoto = document.querySelector('.card__image');
-var uploadMiniPhoto = document.querySelector('.square__white');
-var fr = new FileReader();
+const itemsSocialMedia = document.querySelectorAll('.fill-input__placeholder');
+const uploadBtn = document.querySelector('.fill-input__image');
+const uploadInput = document.querySelector('.fill-input__input');
+const uploadPhoto = document.querySelector('.card__image');
+const uploadMiniPhoto = document.querySelector('.square__white');
+const fr = new FileReader();
 
 
-function reWriteData(e) {
-  var fillInput = e.currentTarget;
-  var targetInput = fillInput.getAttribute('data-stringer');
-  var sendCont = document.querySelector('#' + targetInput);
-  var itemAttribute = fillInput.getAttribute('type');
+const reWriteData = (e) => {
+  const fillInput = e.currentTarget;
+  const targetInput = fillInput.getAttribute('data-stringer');
+  const sendCont = document.querySelector('#' + targetInput);
+  const itemAttribute = fillInput.getAttribute('type');
   if (itemAttribute === 'number') {
     sendCont.href += fillInput.value;
   } else if (itemAttribute === 'email') {
     sendCont.href += fillInput.value;
   } else if (itemAttribute === 'url') {
-    var scriptUrl = fillInput.value + '/';
+    const scriptUrl = fillInput.value + '/';
     sendCont.href += scriptUrl;
   } else {
     sendCont.innerHTML = fillInput.value;
   }
-}
+};
 
 //Cargar Imagen
 
 
-function mostrarImagen(e) {
-  var myFile = e.currentTarget.files[0];
+const mostrarImagen = (e) => {
+  const myFile = e.currentTarget.files[0];
   fr.addEventListener('load', writeImage);
   fr.readAsDataURL(myFile);
-}
+};
 
-function writeImage() {
-  var urlPhoto = 'url(' + '"' + fr.result + '"' + ')';
+const writeImage = () => {
+  const urlPhoto = 'url(' + '"' + fr.result + '"' + ')';
   uploadPhoto.style.backgroundImage = urlPhoto;
   uploadMiniPhoto.style.backgroundImage = urlPhoto;
-}
+};
 
-function clickInput() {
+const clickInput = () => {
   uploadInput.click();
-}
-//console.log(uploadInput);
+};
+
 uploadInput.addEventListener('change', mostrarImagen);
 uploadBtn.addEventListener('click', clickInput);
 
-for (var i = 0; i < itemsSocialMedia.length; i++) {
-  var targetAttribute = itemsSocialMedia[i].getAttribute('type');
+for (const socialMediaItems of itemsSocialMedia.length) {
+  const targetAttribute = socialMediaItems.getAttribute('type');
   if (targetAttribute === 'url') {
-    itemsSocialMedia[i].addEventListener('change', reWriteData);
+    socialMediaItems.addEventListener('change', reWriteData);
   } else {
-    itemsSocialMedia[i].addEventListener('keyup', reWriteData);
+    socialMediaItems.addEventListener('keyup', reWriteData);
   }
 }
 
